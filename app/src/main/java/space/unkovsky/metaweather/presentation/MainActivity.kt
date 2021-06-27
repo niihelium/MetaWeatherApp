4a7e5bcc52e9d659365bc10f0249fa4e1a854263
Nikita Unkovsky <nii.unkovsky@gmail.com>
2021-06-28 01:18:29 +0300

    * Remove wrong fragment commit

--- app/src/main/java/space/unkovsky/metaweather/presentation/MainActivity.kt
+++ app/src/main/java/space/unkovsky/metaweather/presentation/MainActivity.kt
@@ -13,8 +13,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportFragmentManager.commit {
-            add(R.id.container, SearchFragment())
-        }
     }
 }
